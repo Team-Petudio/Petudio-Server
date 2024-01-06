8a7f2806848ece9aa4ec4c835501a3d535183ded
PgmJun <chltmdwns96@gmail.com>
2024-01-07 00:47:18 +0900

    project init

---
+++ src/main/java/com/nice/petudio/PetudioApplication.java
@@ -0,0 +1,13 @@
+package com.nice.petudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nice/petudio/PetudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nice.petudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
