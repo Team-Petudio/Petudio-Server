81861b0657d6dc51230120bf5d7db596e453a5fc
PgmJun <chltmdwns96@gmail.com>
2024-01-17 17:06:06 +0900

    feat: Entity 설계 #3

---
+++ src/main/java/com/nice/petudio/domain/album/Album.java
@@ -0,0 +1,34 @@
+package com.nice.petudio.domain.album;
+
+import com.nice.petudio.domain.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "albums")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Album extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "album_id")
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long member_id;
+
+    @Column(name = "pet_id", nullable = false)
+    private Long pet_id;
+
+    @Column(name = "profile_images", nullable = false)
+    private String profileImages; // JSON type
+}
---
+++ src/main/java/com/nice/petudio/global/config/jpa/JpaAuditingConfig.java
@@ -0,0 +1,7 @@
+package com.nice.petudio.global.config.jpa;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
