6245e574be3a60a286a0585e6009f518ee70edc1
PgmJun <chltmdwns96@gmail.com>
2024-01-19 02:35:46 +0900

    feat: 유저 로직 구현 #8

---
+++ src/main/java/com/nice/petudio/api/controller/member/service/MemberServiceUtils.java
@@ -0,0 +1,33 @@
+package com.nice.petudio.api.controller.member.service;
+
+import com.nice.petudio.domain.member.Member;
+import com.nice.petudio.domain.member.SocialType;
+import com.nice.petudio.domain.member.repository.MemberRepository;
+import com.nice.petudio.global.exception.ConflictException;
+import com.nice.petudio.global.exception.NotFoundException;
+import com.nice.petudio.global.exception.error.ErrorCode;
+import java.util.Optional;
+
+public class MemberServiceUtils {
+
+    public static Member findMemberById(MemberRepository memberRepository, Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MEMBER_EXCEPTION,
+                        String.format("존재하지 않는 memberId(%d) 입니다.", memberId)));
+    }
+
+    static void validateNotExistsMember(MemberRepository memberRepository, String socialId,
+                                        SocialType socialType) {
+        if (memberRepository.existsBySocialIdAndSocialType(socialId, socialType)) {
+            throw new ConflictException(ErrorCode.CONFLICT_MEMBER_EXCEPTION, String.format("이미 존재하는 회원 소셜정보 (%s - %s) 입니다", socialId, socialType));
+        }
+    }
+
+    public static Member findMemberBySocialIdAndSocialType(MemberRepository memberRepository, String socialId,
+                                                           SocialType socialType) {
+        Optional<Member> member = memberRepository.findMemberBySocialIdAndSocialType(socialId, socialType);
+        return member.orElseThrow(() -> new NotFoundException(
+                ErrorCode.NOT_FOUND_MEMBER_EXCEPTION,
+                String.format("존재하지 않는 회원 소셜정보 (%s - %s) 입니다", socialType, socialId)));
+    }
+}
--- src/main/java/com/nice/petudio/domain/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/nice/petudio/domain/member/repository/MemberRepositoryImpl.java
@@ -1,4 +1,44 @@
 package com.nice.petudio.domain.member.repository;
 
+import static com.nice.petudio.domain.member.QMember.member;
+
+import com.nice.petudio.domain.member.Member;
+import com.nice.petudio.domain.member.SocialType;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public boolean existsBySocialIdAndSocialType(String socialId, SocialType socialType) {
+        return Optional.ofNullable(queryFactory.selectOne()
+                .from(member)
+                .where(
+                        member.socialInfo.socialId.eq(socialId),
+                        member.socialInfo.socialType.eq(socialType)
+                )
+                .fetchFirst()).isPresent();
+    }
+
+    @Override
+    public Optional<Member> findMemberById(Long id) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(member)
+                .where(member.id.eq(id))
+                .fetchOne());
+    }
+
+    @Override
+    public Optional<Member> findMemberBySocialIdAndSocialType(String socialId, SocialType socialType) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(member)
+                .where(
+                        member.socialInfo.socialId.eq(socialId),
+                        member.socialInfo.socialType.eq(socialType)
+                )
+                .fetchOne());
+    }
 }
