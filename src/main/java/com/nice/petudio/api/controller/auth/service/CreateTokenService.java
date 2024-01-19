8453ba3fef6fae7c276f8a098ed011e5e5bb3c9c
PgmJun <chltmdwns96@gmail.com>
2024-01-19 14:31:59 +0900

    fix: Jwt 관련 로직 객체명 변경 #8
    Utils는 정적 메서드를 가지고 다양한 곳에서 사용되는 특징을 표현하는 네이밍이지만 해당 객체는 그렇진 않음.
    하지만 Jwt를 활용하기 위한 유틸리티성 기능을 제공하기 때문에 Utils로 네이밍을 결정. 이후 더 괜찮은 네이밍이 있다면 변경할 예정

--- src/main/java/com/nice/petudio/api/controller/auth/service/CreateTokenService.java
+++ src/main/java/com/nice/petudio/api/controller/auth/service/CreateTokenService.java
@@ -5,7 +5,7 @@ import com.nice.petudio.api.controller.auth.vo.TokenVO;
 import com.nice.petudio.api.controller.member.service.MemberServiceUtils;
 import com.nice.petudio.domain.member.Member;
 import com.nice.petudio.domain.member.repository.MemberRepository;
-import com.nice.petudio.global.auth.jwt.JwtTokenService;
+import com.nice.petudio.global.auth.jwt.JwtUtils;
 import com.nice.petudio.global.config.redis.constant.RedisKey;
 import com.nice.petudio.global.exception.model.UnAuthorizedException;
 import com.nice.petudio.global.exception.error.ErrorCode;
@@ -25,19 +25,19 @@ public class CreateTokenService {
 
     private final RedisTemplate redisTemplate;
 
-    private final JwtTokenService jwtTokenService;
+    private final JwtUtils jwtUtils;
 
     public TokenVO createTokenInfo(Long memberId) {
-        List<String> tokens = jwtTokenService.createTokenInfo(memberId);
+        List<String> tokens = jwtUtils.createTokenInfo(memberId);
         return TokenVO.of(tokens.get(0), tokens.get(1));
     }
 
     public TokenVO reissueToken(TokenVO tokenVO) {
-        Long memberId = jwtTokenService.parseMemberId(tokenVO.getAccessToken())
+        Long memberId = jwtUtils.parseMemberId(tokenVO.getAccessToken())
                 .orElseThrow();
         Member member = MemberServiceUtils.findMemberById(memberRepository, memberId);
 
-        if (!jwtTokenService.validateToken(tokenVO.getRefreshToken())) {
+        if (!jwtUtils.validateToken(tokenVO.getRefreshToken())) {
             throw new UnAuthorizedException(ErrorCode.UNAUTHORIZED_JWT_EXCEPTION,
                     String.format("MemberId(%d)의 토큰 갱신 요청에 포함된 Refresh Token이 유효하지 않아, Token Refresh가 수행되지 않았습니다.", memberId));
         }
@@ -47,11 +47,11 @@ public class CreateTokenService {
                     String.format("보관 중인 MemberId(%d)의 Refresh Token이 존재하지 않아, Token Refresh가 수행되지 않았습니다.", memberId));
         }
         if (!refreshToken.equals(tokenVO.getRefreshToken())) {
-            jwtTokenService.expireRefreshToken(member.getId());
+            jwtUtils.expireRefreshToken(member.getId());
             throw new UnAuthorizedException(ErrorCode.UNAUTHORIZED_JWT_EXCEPTION,
                     String.format("보관 중인 MemberId(%d)의 Refresh Token이 유효하지 않아, Token Refresh가 수행되지 않았습니다.", memberId));
         }
-        List<String> tokens = jwtTokenService.createTokenInfo(memberId);
+        List<String> tokens = jwtUtils.createTokenInfo(memberId);
         return TokenVO.of(tokens.get(0), tokens.get(1));
     }
 }
