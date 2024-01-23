37bc2e86e0d07273321b5fbce13a1c0f03658bae
PgmJun <chltmdwns96@gmail.com>
2024-01-23 17:09:58 +0900

    feat: Swagger JWT 설정 #21

--- src/main/java/com/nice/petudio/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/nice/petudio/common/config/swagger/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package com.nice.petudio.common.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +18,18 @@ public class SwaggerConfig {
     private final static String DESCRIPTION = "Petudio API Docs";
     private final static String VERSION = "0.0.1";
 
+    @Value("${deploy.dev.url}")
+    String deployServerUrl;
+
     @Bean
     public OpenAPI openAPI() {
         Server serverLocal = createServer("http://localhost:8080", "for local usages");
+        Server serverDev = createServer(deployServerUrl, "for dev usages");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer Token");
 
         Info info = new Info()
                 .title(TITLE)
@@ -23,7 +37,9 @@ public class SwaggerConfig {
                 .version(VERSION);
 
         return new OpenAPI()
-                .servers(List.of(serverLocal))
+                .components(new Components().addSecuritySchemes("Bearer Token", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .servers(List.of(serverLocal, serverDev))
                 .info(info);
     }
 
