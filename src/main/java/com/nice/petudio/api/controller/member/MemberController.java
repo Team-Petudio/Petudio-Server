62fac67fb5b7bce715185c7dc5df9a2ee6f51239
PgmJun <chltmdwns96@gmail.com>
2024-01-23 16:24:32 +0900

    feat: MemberController API 스웨거 설명 추가 #21

--- src/main/java/com/nice/petudio/api/controller/member/MemberController.java
+++ src/main/java/com/nice/petudio/api/controller/member/MemberController.java
@@ -5,6 +5,7 @@ import com.nice.petudio.api.controller.member.service.MemberCommandService;
 import com.nice.petudio.api.dto.ApiResponse;
 import com.nice.petudio.common.auth.auth.Auth;
 import com.nice.petudio.common.auth.resolver.MemberId;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +22,7 @@ public class MemberController {
     private final MemberCommandService memberCommandService;
 
     @Auth
+    @Operation(summary = "[인증] 유저 회원탈퇴")
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/member/delete")
     public ApiResponse<?> deleteMember(@MemberId final Long memberId) {
@@ -29,6 +31,7 @@ public class MemberController {
     }
 
     @Auth
+    @Operation(summary = "[인증] 유저 알림 수신 상태 변경")
     @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/member/notification/status-change")
     public ApiResponse<ChangeNotificationStatusResponse> changeMemberNotificationStatus(@MemberId final Long memberId,
