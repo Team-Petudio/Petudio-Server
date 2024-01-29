d36dfead84e71a896395ffadcf6823b1ff06119e
PgmJun <chltmdwns96@gmail.com>
2024-01-29 19:12:09 +0900

    remove: DTO의 미사용 필드 제거 #34

--- src/main/java/com/nice/petudio/api/controller/pet/dto/CreatePetImagesUploadUrlsRequest.java
+++ src/main/java/com/nice/petudio/api/controller/pet/dto/CreatePetImagesUploadUrlsRequest.java
@@ -6,7 +6,5 @@ import jakarta.validation.constraints.Min;
 public record CreatePetImagesUploadUrlsRequest(
         @Min(value = 10, message = "이미지 갯수는 10개 이상, 12개 이하이어야 합니다.")
         @Max(value = 12, message = "이미지 갯수는 10개 이상, 12개 이하이어야 합니다.")
-        short imageAmount,
-        String petName
-) {
-}
+        short imageAmount
+) {}
