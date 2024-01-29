bce8cdcad098b3fdedbe2ea1fc3eb25fb36203a7
PgmJun <chltmdwns96@gmail.com>
2024-01-29 17:08:28 +0900

    feat: 반려동물 이미지 저장용 S3 PreSignedURL 요청 기능 구현 #34

--- src/main/java/com/nice/petudio/api/controller/pet/service/PetQueryService.java
+++ src/main/java/com/nice/petudio/api/controller/pet/service/PetQueryService.java
@@ -5,23 +5,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nice.petudio.api.controller.pet.dto.FindMyPetResponse;
 import com.nice.petudio.api.controller.pet.dto.FindMyPetsResponse;
 import com.nice.petudio.api.controller.pet.dto.PetPhotos;
+import com.nice.petudio.api.controller.pet.dto.CreatePetImagesUploadUrlsRequest;
+import com.nice.petudio.api.controller.pet.dto.CreatePetImagesUploadUrlsResponse;
 import com.nice.petudio.common.exception.error.ErrorCode;
 import com.nice.petudio.common.exception.model.InternalServerException;
 import com.nice.petudio.domain.pet.Pet;
 import com.nice.petudio.domain.pet.repository.PetRepository;
+import com.nice.petudio.external.service.S3FileService;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @Slf4j
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class PetQueryService {
     private final PetRepository petRepository;
     private final ObjectMapper objectMapper;
+    private final S3FileService s3FileService;
 
     public FindMyPetsResponse findPetsInfoByMemberId(final Long memberId) {
         List<FindMyPetResponse> petsInfo = new ArrayList<>();
@@ -37,5 +43,14 @@ public class PetQueryService {
             }
         }
         return new FindMyPetsResponse(petsInfo);
+    }
+
+    public CreatePetImagesUploadUrlsResponse createPreSignedUrlForSavePetImages(Long memberId, CreatePetImagesUploadUrlsRequest request) {
+        List<String> preSignedUrls = new ArrayList<>();
+        for(int i = 1; i <= request.imageAmount(); i++) {
+            preSignedUrls.add(s3FileService.getPreSignedUrl(memberId, request.petName(), i));
+        }
+
+        return new CreatePetImagesUploadUrlsResponse(preSignedUrls);
     }
 }
