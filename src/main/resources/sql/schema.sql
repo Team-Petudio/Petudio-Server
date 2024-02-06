ba6464ba271f9f3c92c947f10dabcb0aa4ee2157
PgmJun <chltmdwns96@gmail.com>
2024-02-06 18:24:34 +0900

    feat: 유효 기간 컬럼 추가(1년) #44

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -99,6 +99,7 @@ CREATE TABLE `gifts`
     `user_id`     bigint      NULL,
     `gift_code`   varchar(32) NOT NULL,
     `is_used`     boolean     NOT NULL,
+    `expired_at`  datetime    NOT NULL,
     `created_at`  datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `modified_at` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
