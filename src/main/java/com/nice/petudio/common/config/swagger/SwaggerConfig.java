01aba7e745b18b069a4afae56480b2ec2e41747a
PgmJun <chltmdwns96@gmail.com>
2024-02-09 03:25:27 +0900

    remove: Swagger Server 구분 방식 제거 #46

--- src/main/java/com/nice/petudio/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/nice/petudio/common/config/swagger/SwaggerConfig.java
@@ -6,9 +6,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import java.util.Collections;
-import java.util.List;
 import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,14 +18,9 @@ public class SwaggerConfig {
     private final static String DESCRIPTION = "Petudio API Docs";
     private final static String VERSION = "0.0.1";
 
-    @Value("${deploy.dev.url}")
-    String deployServerUrl;
 
     @Bean
     public OpenAPI openAPI() {
-        Server serverLocal = createServer("http://localhost:8080", "for local usages");
-        Server serverDev = createServer(deployServerUrl, "for dev usages");
-
         SecurityScheme securityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER).name("Authorization");
@@ -41,20 +34,11 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("Bearer Token", securityScheme))
                 .security(Collections.singletonList(securityRequirement))
-                .servers(List.of(serverLocal, serverDev))
                 .info(info);
     }
 
     // API 파라미터 중, MemberId 애노테이션 적용 파라미터는 무시
     static {
         SpringDocUtils.getConfig().addAnnotationsToIgnore(MemberId.class);
-    }
-
-    private Server createServer(String url, String description) {
-        Server server = new Server();
-        server.setUrl(url);
-        server.setDescription(description);
-
-        return server;
     }
 }
