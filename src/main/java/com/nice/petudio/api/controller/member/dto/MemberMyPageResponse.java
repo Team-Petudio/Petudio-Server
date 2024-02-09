27a2d57988f05cc9d6ea27ac0488ab749ca4cfdc
PgmJun <chltmdwns96@gmail.com>
2024-02-09 13:36:11 +0900

    fix: MyPage API 호출 시, memberId 포함 #27

--- src/main/java/com/nice/petudio/api/controller/member/dto/MemberMyPageResponse.java
+++ src/main/java/com/nice/petudio/api/controller/member/dto/MemberMyPageResponse.java
@@ -1,15 +1,18 @@
 package com.nice.petudio.api.controller.member.dto;
 
+import com.nice.petudio.domain.member.Member;
 import com.nice.petudio.domain.member.SocialType;
 import lombok.Builder;
 
 @Builder
-public record MemberMyPageResponse(SocialType socialType, String email, boolean notificationStatus, int ticketAmount) {
-    public static MemberMyPageResponse of(SocialType socialType, String email, boolean notificationStatus, int ticketAmount) {
+public record MemberMyPageResponse(Long memberId, SocialType socialType, String email, boolean notificationStatus,
+                                   int ticketAmount) {
+    public static MemberMyPageResponse of(final Member member, boolean notificationStatus, int ticketAmount) {
         return MemberMyPageResponse
                 .builder()
-                .socialType(socialType)
-                .email(email)
+                .memberId(member.getId())
+                .socialType(member.getSocialType())
+                .email(member.getEmail())
                 .notificationStatus(notificationStatus)
                 .ticketAmount(ticketAmount)
                 .build();
